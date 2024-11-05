23cf990056f98f1b536f5567505bf60e310efc20
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-11-04 22:25:08 -0600

    Added 3 themes and random card generation

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -7,49 +7,76 @@
 
 import SwiftUI
 
+enum themeItems: String{
+    case animals, food, nature
+}
+
+let animals = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦇"]
+let food = ["🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓",]
+let nature = ["🌲", "🌳", "🌿", "🍀", "🍁", "🍄", "🌴", "🌲", "🌿", "🍀"]
+
+// This function first doubles each emoji because the passed in array only contains all unique emojies. Then shuffels them and returns
+
+func getEmojisPairRandomized(emojisArray: [String]) -> [String]{
+    var arrayOfEmojis: [String] = []
+    for index in 0..<emojisArray.count{
+        for _ in 0...1{
+            arrayOfEmojis.append(emojisArray[index])
+        }
+    }
+    arrayOfEmojis.shuffle()
+    return arrayOfEmojis
+}
+
+
+
 struct ContentView: View {
-    @State var cardsTill: Int = 1
-    let emojis = ["🤓", "🤔", "💡", "🤖", "🔥", "🍇"]
+    @State var emojis: [String] = getEmojisPairRandomized(emojisArray: animals)
     var body: some View {
         VStack{
-            Cards
+            Text("Memorize!")
+                .font(.largeTitle)
+            ScrollView{
+                giveCards()
+            }
             Spacer()
-            CardAddingButtons
+            ThemeChoosingButtons
         }.padding()
     }
     
-    var Cards: some View{
-        LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]){
-            ForEach(0..<cardsTill, id: \.self){ index in
+    var ThemeChoosingButtons: some View{
+        HStack(spacing: 40) {
+            ThemeButton(theme: .nature, symbol: "leaf.fill")
+            ThemeButton(theme: .animals, symbol: "pawprint.fill")
+            ThemeButton(theme: .food, symbol: "carrot.fill")
+        }
+    }
+    
+    
+    func ThemeButton(theme: themeItems, symbol: String) -> some View{
+            Button(action: {
+                emojis = getEmojisPairRandomized(emojisArray: theme == .animals ? animals : theme == .food ? food : nature)
+            }){
+                VStack{
+                    Image(systemName: symbol).font(.largeTitle)
+                    Text(theme == .animals ? "Animals" : theme == .food ? "Food" : "Nature").font(.system(size: 20))
+                }
+            }
+    }
+    
+    // Generating the cards in a grid
+    
+    func giveCards() -> some View{
+        return LazyVGrid(columns: [GridItem(), GridItem(), GridItem(), GridItem()]){
+            ForEach(0..<(emojis.count), id: \.self){ index in
                 IndiCard(content:emojis[index])
-                    .aspectRatio(1/1, contentMode: .fit)
+                    .aspectRatio(2/3, contentMode: .fit)
             }.padding(5)
         }
-        
-    }
-    
-    var CardAddingButtons: some View {
-        HStack{
-            CardButtonAdjuster(by: 1, icon: "rectangle.stack.fill.badge.plus")
-                .disabled(cardsTill > emojis.count-1)
-            Spacer()
-            CardButtonAdjuster(by: -1, icon: "rectangle.stack.fill.badge.minus")
-                .disabled(cardsTill < 2)
-        }
-        .padding()
-    }
-    
-    func CardButtonAdjuster(by offset: Int, icon: String) -> some View{
-        Button(action: {
-            cardsTill += offset
-        }, label: {
-            Image(systemName: icon)
-                .imageScale(.large)
-                .font(.title)
-        })
     }
 }
 
+// individual card
 
 struct IndiCard: View{
     var content = ""
